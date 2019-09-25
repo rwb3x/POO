fef3b187470f1bed613b0cd204a18e0e9cb1c6cd
rwb3x <54416678+rwb3x@users.noreply.github.com>
2019-09-24 19:33:22 -0700

    Pra

---
+++ espia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace espia
+{
+    class Duplicador
+    {
+        public void duplica(int x)
+        {
+            x = x*2;
+        }    
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Duplicador dup = new Duplicador();
+            dup.duplica(1);
+
+            int x = 2;
+            dup.duplica(x);
+
+            Console.WriteLine("El valor es: {0}",x);
+            
+        }
+    }
+}
