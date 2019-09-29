b7af5d6ecbafb055ef46f174bcb9982f816c0cc4
Ruben Felix Chavez <ruben.chavez18@tectijuana.edu.mx>
2019-09-29 16:11:50 -0700

    Tarea

--- PeliculasBasic/Program.cs
+++ PeliculasBasic/Program.cs
@@ -4,11 +4,7 @@ namespace PeliculasBasic
 {
     class Pelicula 
     {
-        public String Titulo;
-        public Pelicula
-        {
-            
-        }
+        public String Titulo,Pais,Director;
         public Int16 Año; 
     }
     class Program
@@ -16,13 +12,16 @@ namespace PeliculasBasic
         static void Main(string[] args)
         {
         Pelicula p1 = new Pelicula();
-        p1.SetTitulo("La La Land");
-        p1.SetAño(2016); 
-        Console.WriteLine("{0}({1})", p1.GetTitulo(), p1.GetAño());
+        p1.Titulo="La La Land";
+        p1.Año=2016; 
+        p1.Pais="USA";
+        p1.Director="Austin Gorg";
+        Console.WriteLine("{0}({1})", p1.Titulo, p1.Año);
         Pelicula p2 = new Pelicula();
-        p2.SetTitulo("Green Book");
-        p2.SetAño("2019");
-        Console.WriteLine("{0}({1})", p2.GetTitulo(),p2.GetAño());
+        p2.Titulo="Green Book";
+        p2.Año=2019;
+        p2.Director="Peter Farrelly";
+        Console.WriteLine("{0}({1})", p2.Titulo,p2.Año);
         }
     }
 }
