087f55f2699a69a47160e48876b20ccf3dd892a5
rwb3x <ruben.chavez18@tectijuana.edu.mx>
2019-10-01 19:50:51 -0700

    Update Program.cs

--- GOL/Program.cs
+++ GOL/Program.cs
@@ -2,11 +2,32 @@
 
 namespace GOL
 {
+    enum Estado {
+        viva, muerta
+    }
+    class Tablero
+    {
+        private List<List<Celula>> grid;
+    }
+    class Celula
+    {
+        public Estado estado_actual, estado_siguiente;
+        public short Renglon, Columna;   
+        public Tablero;
+        public Celula (Estado inicial, Tablero tablero, short Renglon, short Columna)
+        {
+            estado_actual= inicial;
+            this.tablero= tablero;
+            this.Renglon= Renglon;
+            this.Columna= Columna;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("▒▒▓");
         }
     }
 }
