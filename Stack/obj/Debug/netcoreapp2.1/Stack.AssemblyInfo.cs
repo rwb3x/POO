6d4c08aa9ba4c1d3f00bae670f19b8a0800feff3
Ruben Felix Chavez <ruben.chavez18@tectijuana.edu.mx>
2019-11-13 20:28:45 -0800

    tarea

--- Stack/obj/Debug/netcoreapp2.1/Stack.AssemblyInfo.cs
+++ Stack/obj/Debug/netcoreapp2.1/Stack.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("espia")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Stack")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("espia")]
-[assembly: System.Reflection.AssemblyTitleAttribute("espia")]
+[assembly: System.Reflection.AssemblyProductAttribute("Stack")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Stack")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
