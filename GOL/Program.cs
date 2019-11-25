40169d5ea6340f5f393937103ec29ac6308f8371
Ruben Felix Chavez <ruben.chavez18@tectijuana.edu.mx>
2019-11-24 20:31:35 -0800

    szdaz

--- GOL/Program.cs
+++ GOL/Program.cs
@@ -9,18 +9,25 @@ namespace GOL
     class Celula
     {
         public Estado estado_actual, estado_siguiente;
-        public short Renglon, Columna;   
-        public Tablero tablero;
-        public Celula (Estado inicial, Tablero tablero, short Renglon, short Columna)
+        public short Renglon, Columna;
+        private Tablero tablero1;
+        public Celula (
+            Estado inicial, Tablero tablero, short Renglon, short Columna)
         {
             estado_actual= inicial;
-            this.tablero= tablero;
+            this.tablero1= tablero;
             this.Renglon= Renglon;
             this.Columna= Columna;
         }
+
+        private Tablero Tablero1 { get => tablero1; set => tablero1 = value; }
+
         public void print()
         {
-        if (this.estado_actual = )
+        if (this.estado_actual = this.viva)
+        {
+            
+        }
         
         }
     class Tablero
